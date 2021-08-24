1a7a7ba70526e7a0c6598f67552fb241b2e6f96e
VAISHNAVI0715 <79103277+VAISHNAVI0715@users.noreply.github.com>
2021-08-24 18:36:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// for 1st random image
+var randomNumber1=Math.floor(Math.random()* 6)+1;
+var randomDiceImage1= "images/dice"+ randomNumber1 +".png";
+var image1= document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1);
+// for 2nd random image
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+var image2= document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="Player 1 wins😁"
+}
+else if(randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML="Player 2 wins👌"
+}
+else
+document.querySelector("h1").innerHTML="Draw!😢"
